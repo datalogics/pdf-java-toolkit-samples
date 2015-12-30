f80a7e1a8ecd67777bc362c521f1eacf66ae5f3f
Dan Strauss <dans@datalogics.com>
2015-12-30 16:39:31 -0600

    Cleaned up code, filled in some fields in PDF form

--- src/main/java/com/datalogics/pdf/samples/manipulation/MergeDocuments.java
+++ src/main/java/com/datalogics/pdf/samples/manipulation/MergeDocuments.java
@@ -9,6 +9,7 @@ import com.adobe.internal.io.InputStreamByteReader;
 import com.adobe.pdfjt.core.exceptions.PDFIOException;
 import com.adobe.pdfjt.core.exceptions.PDFInvalidDocumentException;
 import com.adobe.pdfjt.core.exceptions.PDFSecurityException;
+import com.adobe.pdfjt.core.license.LicenseManager;
 import com.adobe.pdfjt.core.types.ASRectangle;
 import com.adobe.pdfjt.pdf.document.PDFCatalog;
 import com.adobe.pdfjt.pdf.document.PDFDocument;
@@ -27,10 +28,15 @@ import java.io.InputStream;
  * This sample shows how to merge two PDF documents into one. It will also show how to properly merge two PDF documents
  * that contain forms, links, bookmarks, and annotations.
  */
-public class MergeDocuments {
+public final class MergeDocuments {
 
-    private static final String outputFilename = "MergedDocument.pdf";
-    private static final String[] fileNames = { "MergeAnnot.pdf", "MergeForm.pdf", "MergeWatermark.pdf" };
+    private static final String outputPDFPath = "MergedDocument.pdf";
+    private static final String[] fileNames = { "Merge1.pdf", "Merge2.pdf", "Merge3.pdf" };
+
+    /**
+     * This is a utility class, and won't be instantiated.
+     */
+    private MergeDocuments() {}
 
     /**
      * Main program.
@@ -38,84 +44,82 @@ public class MergeDocuments {
      * @param args command line arguments
      * @throws Exception a general exception was thrown
      */
-    public static void main(final String[] args) throws Exception {
-        /*
-         * Start by creating a new PDF document that will be used to merge the other documents into. The new document
-         * will contain a single blank page but we'll remove this just before saving the merged file.
-         */
+    public static void main(final String... args) throws Exception {
+        // If you are using an evaluation version of the product (License Managed, or LM), set the path to where PDFJT
+        // can find the license file.
+        //
+        // If you are not using an evaluation version of the product you can ignore or remove this code.
+        LicenseManager.setLicensePath(".");
+
+        if (args.length > 0) {
+            run(args[0]);
+        } else {
+            // If nothing was passed in, use our default list of files.
+            run(outputPDFPath);
+        }
+    }
+
+    public static void run(final String outputPath) throws Exception {
+        // Start by creating a new PDF document that will be used to merge the other documents into. The new document
+        // will contain a single blank page but we'll remove this just before saving the merged file.
         final PDFDocument mergedDocument = PDFDocument.newInstance(new ASRectangle(new double[] { 0, 0, 612, 792 }),
                                                                    PDFOpenOptions.newInstance());
 
-        /*
-         * Setting the initial view is not required. However, the PMMService which is used to merge the documents will
-         * also merge the bookmarks from the individual files. Setting the initial view to display the bookmarks makes
-         * it easier to see that the sample has worked correctly.
-         */
-        setInitialView(mergedDocument, true, PDFPageLayout.SinglePage,
-                       PDFPageMode.WithBookmarks);
+        // Setting the initial view is not required. However, the PMMService which is used to merge the documents will
+        // also merge the bookmarks from the individual files. Setting the initial view to display the bookmarks makes
+        // it easier to see that the sample has worked correctly.
+        setInitialView(mergedDocument, true, PDFPageLayout.SinglePage, PDFPageMode.WithBookmarks);
 
-        /*
-         * Create the new PMMService that will be used to manipulate the pages.
-         */
+        // Create the new PMMService that will be used to manipulate the pages.
         final PMMService pmmService = new PMMService(mergedDocument);
 
+        InputStream is = null;
+        ByteReader byteReader = null;
+        PDFDocument pdfToAppend = null;
         try {
-            /*
-             * Add the files in the input directory to the new PDF file. This process will append the pages from each
-             * document to the end of the new document creating a continuous series of pages.
-             *
-             * Folders will be skipped.
-             */
-
+            // Add the files in the input directory to the new PDF file. This process will append the pages from each
+            // document to the end of the new document creating a continuous series of pages.
+            // Folders will be skipped.
             for (final String pdfFileName : fileNames) {
-                final InputStream is = MergeDocuments.class.getResourceAsStream(pdfFileName);
+                is = MergeDocuments.class.getResourceAsStream(pdfFileName);
 
-                if (is == null) {
-                    System.out.println("File name is " + pdfFileName);
-                    continue;
-                }
+                byteReader = new InputStreamByteReader(is);
+                pdfToAppend = PDFDocument.newInstance(byteReader, PDFOpenOptions.newInstance());
 
-                final ByteReader byteReader = new InputStreamByteReader(is);
-                final PDFDocument pdfToAppend = PDFDocument.newInstance(byteReader, PDFOpenOptions.newInstance());
-                System.out.println("Pages: " + pdfToAppend.requirePages().getNumPages());
-
-                /*
-                 * Create the Bookmark Title String to imitate the behavior of Acrobat. This will be the title of the
-                 * new bookmark that wraps the bookmarks in the source document before it is added after the last
-                 * bookmark in the merged document. Acrobat uses the basefile name of the source PDF so we'll do that
-                 * too.
-                 */
+                // Create the Bookmark Title String to imitate the behavior of Acrobat. This will be the title of the
+                // new bookmark that wraps the bookmarks in the source document before it is added after the last
+                // bookmark in the merged document. Acrobat uses the basefile name of the source PDF so we'll do that
+                // too.
                 final String documentBookmarkRootName = pdfFileName.substring(0, pdfFileName.length() - 4);
 
-                /*
-                 * Here's the interesting part. PMMOptions control what elements of the source document are copied into
-                 * the target. "All" will copy bookmarks, links, annotations, layer content (though not the layers
-                 * themselves), form fields, and structure.
-                 *
-                 * Form fields with the same name will be merged and assume the value of the first field encountered
-                 * during the appends.
-                 *
-                 * Bookmark destinations and links will be automatically resolved.
-                 */
-                System.out.println("Appending " + pdfFileName + " to the end of " + outputFilename);
+                // Here's the interesting part. PMMOptions control what elements of the source document are copied into
+                // the target. "All" will copy bookmarks, links, annotations, layer content (though not the layers
+                // themselves), form fields, and structure.
+                //
+                // Form fields with the same name will be merged and assume the value of the first field encountered
+                // during the appends.
+                //
+                // Bookmark destinations and links will be automatically resolved.
                 pmmService.appendPages(pdfToAppend, documentBookmarkRootName, PMMOptions.newInstanceAll());
             }
 
-            /*
-             * Remove the first page. We don't need it anymore.
-             */
+            // Remove the first page. We don't need it anymore.
             mergedDocument.requirePages().removePage(mergedDocument.requirePages().getPage(0));
 
-            /*
-             * Save the file
-             */
-            DocumentHelper.saveFullAndClose(mergedDocument, outputFilename);
-            // final RandomAccessFile raf = new RandomAccessFile(outputFilename, "rw");
-            // final ByteWriter byteWriter = new RandomAccessFileByteWriter(raf);
-            // mergedDocument.save(byteWriter, PDFSaveFullOptions.newInstance());
+            // Save the file
+            DocumentHelper.saveFullAndClose(mergedDocument, outputPath);
 
         } finally {
             mergedDocument.close();
+            if (is != null) {
+                is.close();
+            }
+            if (byteReader != null) {
+                byteReader.close();
+            }
+            if (pdfToAppend != null) {
+                pdfToAppend.close();
+            }
         }
 
     }
