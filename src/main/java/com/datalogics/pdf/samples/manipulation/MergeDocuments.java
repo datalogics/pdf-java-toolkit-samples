62c03959c39d2cd7b73dc20c545b200cce618dcc
Dan Strauss <dans@datalogics.com>
2016-01-05 12:11:22 -0600

    Restructured code to append documents
    In order to resolve a PMD error, I moved the file IO and document
    append code to its own function so that it can be easily called for
    each document that we want merged. I also removed one of the three
    input files for simplicity's sake, since the third was redundant.

--- src/main/java/com/datalogics/pdf/samples/manipulation/MergeDocuments.java
+++ src/main/java/com/datalogics/pdf/samples/manipulation/MergeDocuments.java
@@ -18,13 +18,14 @@ import com.datalogics.pdf.document.DocumentHelper;
 import java.io.InputStream;
 
 /**
- * This sample shows how to merge two PDF documents into one. It will also show how to properly merge two PDF documents
- * that contain forms, links, bookmarks, and annotations.
+ * This sample shows how to merge two PDF documents into one. It will also show how to properly merge PDF documents that
+ * contain forms, links, bookmarks, and annotations.
  */
 public final class MergeDocuments {
 
     private static final String outputPDFPath = "MergedDocument.pdf";
-    private static final String[] fileNames = { "Merge1.pdf", "Merge2.pdf", "Merge3.pdf" };
+    private static final String firstDocument = "Merge1.pdf";
+    private static final String secondDocument = "Merge2.pdf";
 
     /**
      * This is a utility class, and won't be instantiated.
@@ -57,46 +58,54 @@ public final class MergeDocuments {
         final PDFDocument mergedDocument = PDFDocument.newInstance(new ASRectangle(ASRectangle.US_LETTER),
                                                                    PDFOpenOptions.newInstance());
 
-        // Create the new PMMService that will be used to manipulate the pages.
-        final PMMService pmmService = new PMMService(mergedDocument);
+        // Append each document to the blank one.
+        appendDocument(firstDocument, mergedDocument);
+        appendDocument(secondDocument, mergedDocument);
 
+        // Remove the first page. We don't need it anymore.
+        mergedDocument.requirePages().removePage(mergedDocument.requirePages().getPage(0));
+
+        // Save the file.
+        DocumentHelper.saveFullAndClose(mergedDocument, outputPath);
+    }
+
+    /**
+     * Append a PDF file to the end of an existing PDF document.
+     *
+     * @param resourceName The name of the resource to be appended
+     * @param pdfDocument The document object to which a new PDF should be appended
+     * @throws Exception a general exception was thrown
+     */
+    private static void appendDocument(final String resourceName, final PDFDocument pdfDocument) throws Exception {
         InputStream is = null;
         ByteReader byteReader = null;
         PDFDocument pdfToAppend = null;
+
+        // Create the new PMMService that will be used to manipulate the pages.
+        final PMMService pmmService = new PMMService(pdfDocument);
+
         try {
-            // Add the input files to the new PDF file. This process will append the pages from each document to the
-            // end of the new document creating a continuous series of pages.
-            for (final String pdfFileName : fileNames) {
-                is = MergeDocuments.class.getResourceAsStream(pdfFileName);
+            // Read in the input file.
+            is = MergeDocuments.class.getResourceAsStream(resourceName);
+            byteReader = new InputStreamByteReader(is);
+            pdfToAppend = PDFDocument.newInstance(byteReader, PDFOpenOptions.newInstance());
 
-                byteReader = new InputStreamByteReader(is);
-                pdfToAppend = PDFDocument.newInstance(byteReader, PDFOpenOptions.newInstance());
+            // Create the Bookmark Title String to imitate the behavior of Acrobat. This will be the title of the
+            // new bookmark that wraps the bookmarks in the source document before it is added after the last
+            // bookmark in the merged document. Acrobat uses the base filename of the source PDF so we'll do that
+            // too.
+            final String documentBookmarkRootName = resourceName.substring(0, resourceName.length() - 4);
 
-                // Create the Bookmark Title String to imitate the behavior of Acrobat. This will be the title of the
-                // new bookmark that wraps the bookmarks in the source document before it is added after the last
-                // bookmark in the merged document. Acrobat uses the basefile name of the source PDF so we'll do that
-                // too.
-                final String documentBookmarkRootName = pdfFileName.substring(0, pdfFileName.length() - 4);
-
-                // PMMOptions control what elements of the source document are copied into the target. "newInstanceAll"
-                // will copy bookmarks, links, annotations, layer content (though not the layers themselves), form
-                // fields, and structure.
-                //
-                // Form fields with the same name will be merged and assume the value of the first field encountered
-                // during the appends.
-                //
-                // Bookmark destinations and links will be automatically resolved.
-                pmmService.appendPages(pdfToAppend, documentBookmarkRootName, PMMOptions.newInstanceAll());
-            }
-
-            // Remove the first page. We don't need it anymore.
-            mergedDocument.requirePages().removePage(mergedDocument.requirePages().getPage(0));
-
-            // Save the file
-            DocumentHelper.saveFullAndClose(mergedDocument, outputPath);
-
+            // PMMOptions control what elements of the source document are copied into the target. "newInstanceAll"
+            // will copy bookmarks, links, annotations, layer content (though not the layers themselves), form
+            // fields, and structure.
+            //
+            // Form fields with the same name will be merged and assume the value of the first field encountered
+            // during the appends.
+            //
+            // Bookmark destinations and links will be automatically resolved.
+            pmmService.appendPages(pdfToAppend, documentBookmarkRootName, PMMOptions.newInstanceAll());
         } finally {
-            mergedDocument.close();
             if (is != null) {
                 is.close();
             }
@@ -107,6 +116,5 @@ public final class MergeDocuments {
                 pdfToAppend.close();
             }
         }
-
     }
 }
