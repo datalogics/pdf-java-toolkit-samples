ced7cb6aa3ce1ae7c18be9c55bcf660e05b64529
Rob Lyman <robl@datalogics.com>
2016-01-12 11:20:47 -0600

    Add first version of unit test
    The test currently calls main() and does nothing else, not even
    asserting results (there's nothing to assert, really).
    
    It also passes an empty argument list, letting the sample print the
    default document.

---
+++ src/test/java/com/datalogics/pdf/samples/printing/PrintPdfTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2015 Datalogics, Inc.
+ */
+
+package com.datalogics.pdf.samples.printing;
+
+import com.datalogics.pdf.samples.SampleTest;
+
+import org.junit.Test;
+
+/**
+ * Tests the PrintPdf sample.
+ */
+public class PrintPdfTest extends SampleTest {
+    @Test
+    public void testMain() throws Exception {
+        final String[] args = new String[0];
+        PrintPdf.main(args);
+    }
+}
