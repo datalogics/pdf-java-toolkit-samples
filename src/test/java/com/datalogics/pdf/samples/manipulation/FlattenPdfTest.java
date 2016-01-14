8a4ff4ce3ca023161272b3b9430a1ac8c5de879c
Taylor Smith <tsmith@datalogics.com>
2016-01-14 10:28:12 -0600

    Add a test for flattening annotations
    Along with an annotation test file

--- src/test/java/com/datalogics/pdf/samples/manipulation/FlattenPdfTest.java
+++ src/test/java/com/datalogics/pdf/samples/manipulation/FlattenPdfTest.java
@@ -8,6 +8,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import com.adobe.pdfjt.pdf.document.PDFDocument;
+import com.adobe.pdfjt.pdf.interactive.annotation.PDFAnnotationList;
+import com.adobe.pdfjt.pdf.page.PDFPage;
+import com.adobe.pdfjt.pdf.page.PDFPageTree;
 
 import com.datalogics.pdf.samples.SampleTest;
 
@@ -23,6 +26,7 @@ public class FlattenPdfTest extends SampleTest {
 
     private static final String OUTPUT_FLATTENED_PDF_PATH = "Flattened.pdf";
     private static final String INPUT_FORM_PDF_PATH = "Acroform.pdf";
+    private static final String INPUT_ANNOTATION_PDF_PATH = "annotations.pdf";
 
     @Test
     public void testFlattenForm() throws Exception {
@@ -42,6 +46,24 @@ public class FlattenPdfTest extends SampleTest {
 
     @Test
     public void testFlattenAnnotations() throws Exception {
+        final File file = newOutputFile(OUTPUT_FLATTENED_PDF_PATH);
+        if (file.exists()) {
+            Files.delete(file.toPath());
+        }
+
+        FlattenPdf.main(INPUT_ANNOTATION_PDF_PATH, file.getCanonicalPath());
+        assertTrue(file.getPath() + " must exist after run", file.exists());
+
+        final PDFDocument document = openPdfDocument(file.getCanonicalPath());
+
+        final PDFPageTree pgTree = document.requirePages();
+        final PDFPage currentPage = pgTree.getPage(0);
+
+        final PDFAnnotationList list = currentPage.getAnnotationList();
+
+        for (int i = 0; i < list.size(); i++) {
+            assertEquals("Flattened annotations should not have content", list.get(i).getContents(), null);
+        }
 
     }
 
