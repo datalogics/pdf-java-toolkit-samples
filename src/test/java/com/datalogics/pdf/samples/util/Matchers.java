2e35cf5cf61dbef49cf87268c639ce83f0e62cba
Rob Lyman <robl@datalogics.com>
2016-01-15 14:01:18 -0600

    Add Hamcrest Matcher for InputStreams
    This extends the toolset we have for checking PDFXObjectImages to
    check images contained in InputStreams as well.

--- src/test/java/com/datalogics/pdf/samples/util/Matchers.java
+++ src/test/java/com/datalogics/pdf/samples/util/Matchers.java
@@ -15,6 +15,7 @@ import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.NoSuchAlgorithmException;
 
 /**
@@ -47,6 +48,34 @@ public class Matchers {
                     throwChecksumError(e);
                 } catch (final PDFSecurityException e) {
                     throwChecksumError(e);
+                } catch (final NoSuchAlgorithmException e) {
+                    throwChecksumError(e);
+                } catch (final IOException e) {
+                    throwChecksumError(e);
+                }
+                return null;
+            }
+        };
+    }
+
+    /**
+     * Check that an {@link InputStream} containing image data has a particular SHA-1 checksum.
+     *
+     * @param checksum the image checksum to check for
+     * @return a {@link Matcher}
+     */
+    public static Matcher<InputStream> inputStreamHasChecksum(final String checksum) {
+        // see http://www.planetgeek.ch/2012/03/07/create-your-own-matcher/ for an explanation
+        return new FeatureMatcher<InputStream, String>(equalTo(checksum), "has checksum", "checksum") {
+            private void throwChecksumError(final Throwable exc) {
+                throw new IllegalStateException("Getting an image checksum threw " + exc, exc);
+            }
+
+            @Override
+            protected String featureValueOf(final InputStream imageStream) {
+                final InputStream stream = imageStream;
+                try {
+                    return Checksum.getSha1Checksum(stream);
                 } catch (final NoSuchAlgorithmException e) {
                     throwChecksumError(e);
                 } catch (final IOException e) {
