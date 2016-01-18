d7422af8a9fa9b6f04339b8e21858164af1018c8
Taylor Smith <tsmith@datalogics.com>
2016-01-18 15:10:20 -0600

    Update the description of the flatten sample
    This was written before we realized that it does not actually
    handle layers and so that needs to be removed.
    
    It was also requested that the sample be more explicit about
    the fact that ALL flattening (currently just forms and 
    annotations) is done through the formFlattener.

--- src/main/java/com/datalogics/pdf/samples/manipulation/FlattenPdf.java
+++ src/main/java/com/datalogics/pdf/samples/manipulation/FlattenPdf.java
@@ -31,7 +31,9 @@ import java.util.EnumSet;
 
 /**
  * This sample demonstrates how to use the flattening functions to flatten the interactive elements of a PDF. The
- * flattening functionality currently handles forms, annotations, and layers. It does not currently flatten
+ * flattening functionality currently handles forms and annotations. All flattening is done through the FormFlattener's
+ * flattenDocument() method, and specific element types can be flattened or ignored by specifying them in the
+ * setAnnotationsToBeProcessed() method of the APContext passed in. Note that PDFJT does not currently flatten
  * transparency.
  */
 public final class FlattenPdf {
