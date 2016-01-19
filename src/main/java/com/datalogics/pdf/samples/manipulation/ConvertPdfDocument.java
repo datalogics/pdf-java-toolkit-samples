8638d11025cddf73c5517c6abfee0dab475560f5
jrowden8 <jrowden@datalogics.com>
2016-01-19 16:01:20 -0600

    Create sample that demos how to convert to PDF/A-1b.

---
+++ src/main/java/com/datalogics/pdf/samples/manipulation/ConvertPdfDocument.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2015 Datalogics, Inc.
+ */
+
+package com.datalogics.pdf.samples.manipulation;
+
+
+import com.adobe.internal.io.ByteReader;
+import com.adobe.internal.io.InputStreamByteReader;
+import com.adobe.pdfjt.core.exceptions.PDFFontException;
+import com.adobe.pdfjt.core.exceptions.PDFIOException;
+import com.adobe.pdfjt.core.exceptions.PDFInvalidDocumentException;
+import com.adobe.pdfjt.core.exceptions.PDFInvalidParameterException;
+import com.adobe.pdfjt.core.exceptions.PDFSecurityException;
+import com.adobe.pdfjt.core.exceptions.PDFUnableToCompleteOperationException;
+import com.adobe.pdfjt.core.fontset.PDFFontSet;
+import com.adobe.pdfjt.core.fontset.PDFFontSetManager;
+import com.adobe.pdfjt.core.license.LicenseManager;
+import com.adobe.pdfjt.pdf.document.PDFDocument;
+import com.adobe.pdfjt.pdf.document.PDFOpenOptions;
+
+import com.datalogics.pdf.samples.util.ConvertPdfA1Util;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+/**
+ * Sample that demonstrates how to convert a PDF to PDF/A-1b.
+ */
+public final class ConvertPdfDocument {
+
+    private static final String INPUT_UNCONVERTED_PDF_PATH = "UnConvertedPdf.pdf";
+    private static final String OUTPUT_CONVERTED_PDF_PATH = "ConvertedPdfa-1b.pdf";
+
+
+    /**
+     * This is a utility class, and won't be instantiated.
+     */
+    private ConvertPdfDocument() {}
+
+    /**
+     * Main program.
+     *
+     * @param args command line arguments
+     * @throws Exception a general exception was thrown
+     */
+    public static void main(final String... args) throws Exception {
+        // If you are using an evaluation version of the product (License Managed, or LM), set the path to where PDFJT
+        // can find the license file.
+        //
+        // If you are not using an evaluation version of the product you can ignore or remove this code.
+        LicenseManager.setLicensePath(".");
+        String path;
+        if (args.length > 0) {
+            path = args[0];
+        } else {
+            path = OUTPUT_CONVERTED_PDF_PATH;
+        }
+
+        convertToPdfA1(path);
+    }
+
+    private static void convertToPdfA1(final String outputPath)
+                    throws IOException, PDFFontException, PDFInvalidDocumentException, PDFIOException,
+                    PDFSecurityException, PDFInvalidParameterException, PDFUnableToCompleteOperationException {
+
+        PDFDocument pdfDoc = null;
+        ByteReader reader = null;
+
+        final InputStream inputStream = ConvertPdfDocument.class.getResourceAsStream(INPUT_UNCONVERTED_PDF_PATH);
+        reader = new InputStreamByteReader(inputStream);
+
+        // attach font set to PDF
+        final PDFFontSet pdfaFontSet = PDFFontSetManager.getPDFFontSetInstance();
+        final PDFOpenOptions openOptions = PDFOpenOptions.newInstance();
+        openOptions.setFontSet(pdfaFontSet);
+
+        pdfDoc = PDFDocument.newInstance(reader, openOptions);
+
+        // Note: Transparency is not handled by PDF Java Toolkit
+        ConvertPdfA1Util.convertPdfA1(pdfDoc, outputPath);
+
+    }
+}
