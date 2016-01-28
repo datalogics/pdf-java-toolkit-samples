495b846ef057980053843701900037047a50ea39
Kevin A. Mitchell <kam@datalogics.com>
2016-01-27 23:13:22 -0600

    Integration tests: Mock a fake printer
    - Allows tests that do printing to work without wasting paper, or
      getting exceptions on test servers that don't have printers defined.

--- src/test/java/com/datalogics/pdf/samples/RunMainMethodsFromJarIntegrationTest.java
+++ src/test/java/com/datalogics/pdf/samples/RunMainMethodsFromJarIntegrationTest.java
@@ -9,6 +9,13 @@ import static org.reflections.ReflectionUtils.withModifier;
 import static org.reflections.ReflectionUtils.withName;
 import static org.reflections.ReflectionUtils.withParameters;
 
+import com.datalogics.pdf.samples.printing.FakePrintService;
+import com.datalogics.pdf.samples.printing.FakePrinterJob;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import mockit.Mock;
+import mockit.MockUp;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -18,12 +25,16 @@ import org.reflections.scanners.SubTypesScanner;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
 
+import java.awt.print.PrinterJob;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Set;
+
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
 
 /**
  * As an integration test, run all the main methods in the {@code com.datalogics.pdf.samples} package with an empty
@@ -35,6 +46,9 @@ import java.util.Set;
  * when called from the jar with no arguments.
  *
  */
+@SuppressFBWarnings(value = { "SIC_INNER_SHOULD_BE_STATIC_ANON", "UMAC_UNCALLABLE_METHOD_OF_ANONYMOUS_CLASS" },
+                    justification = "JMockit coding pattern depends on anonymous classes "
+                                    + "and methods with no discernable call site")
 @RunWith(Parameterized.class)
 public class RunMainMethodsFromJarIntegrationTest {
     Method mainMethod;
@@ -98,7 +112,25 @@ public class RunMainMethodsFromJarIntegrationTest {
      * @throws Exception a general exception was thrown
      */
     @Test
-    public void testMainProgram() throws Exception {
+    public <T extends PrinterJob> void testMainProgram() throws Exception {
+        // Set up fake printing, so that anything that tries to print goes to the
+        // printer equivalent of /dev/null
+        new MockUp<PrintServiceLookup>() {
+            @Mock
+            PrintService lookupDefaultPrintService() {
+                return new FakePrintService();
+            }
+        };
+
+        // Mock the PrinterJob.getPrinterJob() method to return a TestPrinterJob object
+        new MockUp<T>() {
+            @Mock
+            public PrinterJob getPrinterJob() {
+                return new FakePrinterJob();
+            }
+        };
+
+        // Invoke the main method of that class
         mainMethod.invoke(null, new Object[] { new String[] {} });
     }
 
