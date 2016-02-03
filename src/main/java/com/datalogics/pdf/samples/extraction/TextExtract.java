e8d9a190a7a8b50298248963f1d76778e0058222
Vel Genov <velg@datalogics.com>
2016-02-03 15:14:35 -0600

    Refactored the TextExtract sample to follow to agreed upon design pattern.
    The main method should handle only file I/O, and generate URLs for files.
    The business logic should handle the rest.

--- src/main/java/com/datalogics/pdf/samples/extraction/TextExtract.java
+++ src/main/java/com/datalogics/pdf/samples/extraction/TextExtract.java
@@ -8,6 +8,7 @@ import com.adobe.pdfjt.core.exceptions.PDFFontException;
 import com.adobe.pdfjt.core.exceptions.PDFIOException;
 import com.adobe.pdfjt.core.exceptions.PDFInvalidDocumentException;
 import com.adobe.pdfjt.core.exceptions.PDFSecurityException;
+import com.adobe.pdfjt.core.exceptions.PDFUnableToCompleteOperationException;
 import com.adobe.pdfjt.core.fontset.PDFFontSet;
 import com.adobe.pdfjt.core.license.LicenseManager;
 import com.adobe.pdfjt.pdf.document.PDFDocument;
@@ -22,6 +23,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 
 /**
@@ -30,7 +33,7 @@ import java.nio.file.Files;
  */
 public final class TextExtract {
     private static final String INPUT_PDF_PATH = "/com/datalogics/pdf/samples/pdfjavatoolkit-ds.pdf";
-    private static final String OUTPUT_TEXT_PATH = "TextExtract.txt";
+    private static final String OUTPUT_TEXT_PATH = "Text Extract.txt";
 
     /**
      * This is a utility class, and won't be instantiated.
@@ -49,60 +52,64 @@ public final class TextExtract {
         //
         // If you are not using an evaluation version of the product you can ignore or remove this code.
         LicenseManager.setLicensePath(".");
-        String inputPath = null;
-        String outputPath = null;
-        PDFDocument document = null;
-        FileOutputStream outputStream = null;
+        URL inputUrl = null;
+        URL outputUrl = null;
 
         if (args.length > 1) {
-            inputPath = args[0];
-            outputPath = args[1];
+            inputUrl = new URL(args[0]);
+            outputUrl = new URL(args[1]);
         } else {
-            inputPath = INPUT_PDF_PATH;
-            outputPath = OUTPUT_TEXT_PATH;
+            inputUrl = TextExtract.class.getResource(INPUT_PDF_PATH);
+            outputUrl = new File(OUTPUT_TEXT_PATH).toURI().toURL();
         }
 
-        try {
-            final File outputFile = new File(outputPath);
-            if (outputFile.exists()) {
-                Files.delete(outputFile.toPath());
-            }
-            outputStream = new FileOutputStream(outputFile);
-            document = DocumentUtils.openPdfDocument(inputPath);
-            extractTextReadingOrder(document, outputStream);
-        } finally {
-            if (document != null) {
-                document.close();
-            }
-
-            if (outputStream != null) {
-                outputStream.close();
-            }
-        }
+        extractTextReadingOrder(inputUrl, outputUrl);
     }
 
     /**
      * Extracts the text from a PDF file in reading order.
      *
-     * @param document the input document, to extract text from
-     * @param outputStream the file stream containing the extracted text
+     * @param inputUrl An URL for the input document, to extract text from
+     * @param outputUrl An URL for the file stream where the extracted text will be written
      * @throws PDFInvalidDocumentException a general problem with the PDF document, which may now be in an invalid state
      * @throws PDFIOException there was an error reading or writing a PDF file or temporary caches
      * @throws PDFFontException there was an error in the font set or an individual font
      * @throws PDFSecurityException some general security issue occurred during the processing of the request
      * @throws UnsupportedEncodingException the character encoding is not supported
      * @throws IOException an I/O operation failed or was interrupted
+     * @throws PDFUnableToCompleteOperationException the operation was unable to be completed
      */
-    private static void extractTextReadingOrder(final PDFDocument document, final FileOutputStream outputStream)
+    public static void extractTextReadingOrder(final URL inputUrl, final URL outputUrl)
                     throws PDFInvalidDocumentException, PDFIOException, PDFFontException, PDFSecurityException,
-                    UnsupportedEncodingException, IOException {
-        final PDFFontSet docFontSet = FontUtils.getDocFontSet(document);
-        final ReadingOrderTextExtractor extractor = ReadingOrderTextExtractor.newInstance(document, docFontSet);
-        final WordsIterator wordsIter = extractor.getWordsIterator();
+                    UnsupportedEncodingException, IOException, PDFUnableToCompleteOperationException {
+        File outputFile = null;
+        try {
+            outputFile = new File(outputUrl.toURI());
+        } catch (final URISyntaxException e) {
+            throw new PDFIOException(e);
+        }
 
-        while (wordsIter.hasNext()) {
-            final Word word = wordsIter.next();
-            outputStream.write(word.toString().getBytes("UTF-8"));
+        if (outputFile.exists()) {
+            Files.delete(outputFile.toPath());
+        }
+
+        PDFDocument document = null;
+        try (final FileOutputStream outputStream = new FileOutputStream(outputFile)) {
+            document = DocumentUtils.openPdfDocument(inputUrl);
+
+            final PDFFontSet docFontSet = FontUtils.getDocFontSet(document);
+            final ReadingOrderTextExtractor extractor = ReadingOrderTextExtractor.newInstance(document, docFontSet);
+            final WordsIterator wordsIter = extractor.getWordsIterator();
+
+            while (wordsIter.hasNext()) {
+                final Word word = wordsIter.next();
+                outputStream.write(word.toString().getBytes("UTF-8"));
+            }
+
+        } finally {
+            if (document != null) {
+                document.close();
+            }
         }
     }
 }
