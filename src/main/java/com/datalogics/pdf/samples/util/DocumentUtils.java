1790b20ba112b91f75a712a71aa35c1ff38246b2
Vel Genov <velg@datalogics.com>
2016-02-04 14:47:52 -0600

    Added the openPdfDocumentWithOptions method to DocumentUtils

--- src/main/java/com/datalogics/pdf/samples/util/DocumentUtils.java
+++ src/main/java/com/datalogics/pdf/samples/util/DocumentUtils.java
@@ -60,12 +60,30 @@ public final class DocumentUtils {
      */
     public static PDFDocument openPdfDocument(final URL inputUrl)
                     throws IOException, PDFInvalidDocumentException, PDFIOException, PDFSecurityException {
+        final PDFOpenOptions pdfOpenOptions = PDFOpenOptions.newInstance();
+
+        return openPdfDocumentWithOptions(inputUrl, pdfOpenOptions);
+    }
+
+    /**
+     * Open a PDF file using an input path.
+     *
+     * @param inputUrl The URL to a PDF file to open
+     * @param pdfOpenOptions Options used to open a PDF document
+     * @return A new PDFDocument instance of the input document
+     * @throws PDFInvalidDocumentException a general problem with the PDF document, which may now be in an invalid state
+     * @throws PDFIOException there was an error reading or writing a PDF file or temporary caches
+     * @throws PDFSecurityException some general security issue occurred during the processing of the request
+     * @throws IOException an I/O operation failed or was interrupted
+     */
+    public static PDFDocument openPdfDocumentWithOptions(final URL inputUrl, final PDFOpenOptions pdfOpenOptions)
+                    throws IOException, PDFInvalidDocumentException, PDFIOException, PDFSecurityException {
         ByteReader reader = null;
         PDFDocument document = null;
 
         final InputStream inputStream = inputUrl.openStream();
         reader = new InputStreamByteReader(inputStream);
-        document = PDFDocument.newInstance(reader, PDFOpenOptions.newInstance());
+        document = PDFDocument.newInstance(reader, pdfOpenOptions);
 
         return document;
     }
