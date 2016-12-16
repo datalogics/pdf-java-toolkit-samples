40397c1af997f6ba16a7b6a6625b891a256f894b
Mark Dawson <mdawson@datalogics.com>
2016-12-16 11:00:29 -0600

    Corrected formatting to fix format
    Forgot to add <p> character
    
    MakeWhiteFangbook.java sample program

--- src/main/java/com/datalogics/pdf/samples/creation/MakeWhiteFangBook.java
+++ src/main/java/com/datalogics/pdf/samples/creation/MakeWhiteFangBook.java
@@ -40,6 +40,7 @@ import java.net.URL;
 import javax.imageio.ImageIO;
 
 /**
+ * <p>
  * This sample creates a short version of the book White Fang. It uses PDFJT to insert a picture on the first page, then
  * Talkeetna to flow the text of chapter one. The sample also adds headers and formats paragraphs, while Talkeetna provides
  * default settings for the line height, font size and style, and page margins.  
