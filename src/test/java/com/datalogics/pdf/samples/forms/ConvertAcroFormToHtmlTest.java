0c3a80f9d5e4d293cdfb29dc0237ca7e746524ef
Brandon Haugen <bhaugen@datalogics.com>
2017-06-05 14:35:55 -0500

    Check title element in Head of Html output

--- src/test/java/com/datalogics/pdf/samples/forms/ConvertAcroFormToHtmlTest.java
+++ src/test/java/com/datalogics/pdf/samples/forms/ConvertAcroFormToHtmlTest.java
@@ -4,10 +4,15 @@
 
 package com.datalogics.pdf.samples.forms;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import com.datalogics.pdf.samples.SampleTest;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 
 import java.io.File;
@@ -17,6 +22,7 @@ import java.net.URL;
  * Tests the ConvertAcroFormToHtml sample.
  */
 public class ConvertAcroFormToHtmlTest extends SampleTest {
+    private static final String HTML_TITLE = "FruitForm_1";
     @Test
     public void testConvertAcroFormToHtmlWithDefaultSampleInput() throws Exception {
         final URL inputUrl = ConvertAcroFormToHtml.class.getResource(ConvertAcroFormToHtml.DEFAULT_INPUT);
@@ -25,5 +31,13 @@ public class ConvertAcroFormToHtmlTest extends SampleTest {
         ConvertAcroFormToHtml.createHtmlForm(inputUrl, outputHtmlFile.toURI().toURL());
         assertTrue(outputHtmlFile.getPath() + " must exist after run", outputHtmlFile.exists());
 
+        final Document htmlDocument = Jsoup.parse(outputHtmlFile, "UTF-8");
+
+        final Elements headTitleElements = htmlDocument.head().getElementsByTag("title");
+        assertEquals("Only one title element should be found in the head of the Html",
+                     headTitleElements.size(), 1);
+
+        final Element headTitle = headTitleElements.get(0);
+        assertEquals("Title of the Html output should be " + HTML_TITLE, HTML_TITLE, headTitle.text());
     }
 }
