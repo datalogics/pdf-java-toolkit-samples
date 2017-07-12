8dac5535e2e312c0a059697f338e2660fc0e3543
Corey Yates <coreyy@datalogics.com>
2017-07-12 10:31:19 -0500

    Test the ConvertToPdfA2 sample
    - Runs the sample and checks the validity of the converted document

---
+++ src/test/java/com/datalogics/pdf/samples/manipulation/ConvertToPdfA2Test.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2017 Datalogics, Inc.
+ */
+
+package com.datalogics.pdf.samples.manipulation;
+
+import static org.junit.Assert.assertTrue;
+
+import com.adobe.pdfjt.services.pdfa2.PDFA2ConformanceLevel;
+
+import com.datalogics.pdf.samples.SampleTest;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URL;
+
+/**
+ * Tests the ConvertToPdfA2 sample.
+ */
+public class ConvertToPdfA2Test extends SampleTest {
+
+    private static final String FILE_NAME = "ConvertedPdfa-2b.pdf";
+
+    @Test
+    public void testConvertToPdfA2B() throws Exception {
+        final URL inputUrl = ConvertToPdfA2.class.getResource(ConvertToPdfA2.INPUT_PDF_PATH);
+        final File outputFile = newOutputFileWithDelete(FILE_NAME);
+        final URL outputUrl = outputFile.toURI().toURL();
+
+        ConvertToPdfA2.convertToPdfA2(inputUrl, outputUrl, PDFA2ConformanceLevel.Level_2b);
+        // Make sure the Output file exists.
+        assertTrue(outputFile.getPath() + " must exist after run", outputFile.exists());
+
+        // Validate the converted document
+        final boolean validity = ConvertToPdfA2.validate(outputUrl, PDFA2ConformanceLevel.Level_2b);
+        assertTrue(outputFile.getPath() + " must be pdfa2-b compliant", validity);
+    }
+}
