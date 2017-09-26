c86ef93845561b82608d414b53257384f2d71754
Kevin A. Mitchell <kam@datalogics.com>
2017-09-26 14:34:36 -0500

    RenderPdfTest: Rework to be parameterized.
    - Reports a checksum error for each output file individually.
    - Already-existing PNG files test the same.

--- src/test/java/com/datalogics/pdf/samples/rendering/RenderPdfTest.java
+++ src/test/java/com/datalogics/pdf/samples/rendering/RenderPdfTest.java
@@ -12,13 +12,19 @@ import static org.junit.Assert.assertTrue;
 import com.datalogics.pdf.samples.SampleTest;
 
 import org.apache.commons.lang3.SystemUtils;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.imageio.ImageIO;
 
@@ -26,56 +32,85 @@ import javax.imageio.ImageIO;
 /**
  * Test the RenderPdf sample.
  */
+@RunWith(Parameterized.class)
 public class RenderPdfTest extends SampleTest {
 
+    private static final String CLASS_NAME = MethodHandles.lookup().lookupClass().getSimpleName();
     private static final int RESOLUTION = 72;
-    private static final String[] CHECKSUMS;
+    private static Boolean renderDone;
 
-    static {
-        // Different versions of Java render some colors differently
-        if (SystemUtils.IS_JAVA_1_8) {
-            CHECKSUMS = new String[] {
-                "", // dummy
-                "359806f590dee0e642a10b9b5043e46fd74fa3ce", // page 1
-                "3192548a1abf89fec8de8d8b38f906d8717613b8" // page 2
-            };
-        } else {
-            CHECKSUMS = new String[] {
-                "", // dummy
-                "1992474437f5b1ee5a885322fb089915a57fe8c9", // page 1
-                "560bae832b056507eac24c81d6f6ef65d2685667" // page 2
-            };
 
-        }
+    /**
+     * Generate a list of parameters for the test. These are a combination of file names and checksums.
+     *
+     * @return the list of parameters for the parameterized tests.
+     */
+    @Parameters(name = "{0}")
+    public static Iterable<Object[]> parameters() throws IOException {
+        return new ArrayList<Object[]>() {
+            private static final long serialVersionUID = 7576159003442840992L;
 
+            private void add(final String filename, final String checksum) throws IOException {
+                final File file = newOutputFileWithDelete(filename);
+                add(new Object[] { filename, file, checksum });
+            }
+
+            {
+                if (SystemUtils.IS_JAVA_1_8) {
+                    add(CLASS_NAME + ".1.png", "359806f590dee0e642a10b9b5043e46fd74fa3ce");
+                    add(CLASS_NAME + ".2.png", "3192548a1abf89fec8de8d8b38f906d8717613b8");
+                } else {
+                    add(CLASS_NAME + ".1.png", "1992474437f5b1ee5a885322fb089915a57fe8c9");
+                    add(CLASS_NAME + ".2.png", "560bae832b056507eac24c81d6f6ef65d2685667");
+                }
+            }
+        };
     }
 
-    @Test
-    public void testRenderPdf() throws Exception {
-        final URL inputUrl = RenderPdf.class.getResource(RenderPdf.DEFAULT_INPUT);
-        final List<File> files = new ArrayList<>();
-        files.add(null); // dummy for 0 to 1 based indexing
+    @Parameter
+    public String fileName;
 
-        final String outputBaseName = getClass().getSimpleName();
-        for (int i = 1; i <= 2; i++) {
-            final File outputFile = newOutputFileWithDelete(outputBaseName + "." + i + ".png");
-            files.add(outputFile);
+    @Parameter(1)
+    public File outputFile;
+
+    @Parameter(2)
+    public String checksum;
+
+
+    @BeforeClass
+    public static void setUpClass() {
+        renderDone = false;
+    }
+
+    private static void ensureRender() throws Exception {
+        if (renderDone) {
+            return;
         }
 
+        final URL inputUrl = RenderPdf.class.getResource(RenderPdf.DEFAULT_INPUT);
+
         // This is the base filename, to which will be appended the page number and the .png extension
-        final URL outputUrl = newOutputFile(outputBaseName).toURI().toURL();
+        final URL outputUrl = newOutputFile(CLASS_NAME).toURI().toURL();
 
         RenderPdf.renderPdf(inputUrl, RESOLUTION, outputUrl);
 
-        for (int i = 1; i <= 2; i++) {
-            final File outputFile = files.get(i);
+        renderDone = true;
+    }
 
-            // Make sure the Output file exists.
-            assertTrue(outputFile.getPath() + " must exist after run", outputFile.exists());
+    /**
+     * Check that the image checksum for a page matches.
+     *
+     * @throws Exception a general exception was thrown
+     */
+    @Test
+    public void imageChecksumMatches() throws Exception {
+        ensureRender();
 
-            // and has the correct checksum
-            final BufferedImage image = ImageIO.read(outputFile);
-            assertThat("Page " + i + " has correct checksum", image, bufferedImageHasChecksum(CHECKSUMS[i]));
-        }
+        // Make sure the Output file exists.
+        assertTrue(outputFile.getPath() + " must exist after run", outputFile.exists());
+
+        // and has the correct checksum
+        final BufferedImage image = ImageIO.read(outputFile);
+        assertThat("File " + fileName + " has correct checksum", image, bufferedImageHasChecksum(checksum));
     }
 }
